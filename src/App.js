e1be9dc55841146a048b2231eb63164ea577f34e
Zohre Salimi <zohre.1987@gmail.com>
2021-02-04 17:45:19 +0100

    add Mirag package and create server by Miraj, implement useAxiox hook for api call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
+import { Server } from "miragejs";
+
+import myData from "./mockProducts.json";
+import useAxios from "./hooks/useAxios";
+
+new Server({
+  routes() {
+    this.namespace = "api";
+    this.get("/products", () => {
+      return myData;
+    });
+  },
+});
+
 function App() {
+  const [{ data, isLoading, isError }, doFetch] = useAxios();
+  console.log("data", data);
+  console.log("isLoading", isLoading);
+  console.log("isError", isError);
+  console.log("doFetch", doFetch);
   return <div className="App">App</div>;
 }
 
