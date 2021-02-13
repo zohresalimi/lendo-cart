3d7269b0b56321803dd88196d784c4562e0c0d06
Zohre Salimi <zohre.1987@gmail.com>
2021-02-13 12:57:30 +0100

    add comment

--- src/store/state.js
+++ src/store/state.js
@@ -1,3 +1,10 @@
+/**
+ * I decided to restructure the state and
+ * group products by ID and color, because
+ * we will have to access products with both
+ * two factors in our components and to control
+ * the quantity by this field
+ */
 const state = {
   products: {
     items: [],
